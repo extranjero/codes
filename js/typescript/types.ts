00609dbd5ff118373ff396cdd53b2db61067973f
Abdullo <ahidoyatov@gmail.com>
2015-07-30 19:04:12 +0500

    Type script types

---
+++ js/typescript/types.ts
@@ -0,0 +1,40 @@
+//Boolean
+var isDone: boolean = false;
+
+//Float
+var float: number = 6;
+
+//String
+var str: string = "my string";
+
+//Arrays
+var list:number[] = [1,2,3];
+var list2:Array<number> = [1,2,3];
+
+//Enum
+enum Color {Red, Green, Blue};
+
+//Bydefault enum start with 0 but you can change it
+enum Color1 {Red=1, Green, Blue};
+
+//Or set your
+enum Color2 {Red=1, Green=3, Blue=4};
+
+var c: Color = Color.Green;
+
+//any type
+var notSure : any = 4;
+notSure = false;
+
+//Void
+
+function warnUser() :void {
+	console.log("Bugaga");
+}	
+
+
+
+
+
+
+
